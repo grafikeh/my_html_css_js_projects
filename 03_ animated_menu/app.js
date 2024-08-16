4940f82c6a046504e640305a517b77a23f0613e2
grafikeh <grafikeh@gmail.com>
2024-08-16 12:01:50 +0300

    Update app.js

--- 03_ animated_menu/app.js
+++ 03_ animated_menu/app.js
@@ -1,12 +1,19 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const container = document.querySelector('.container');
+const open = document.getElementById("open"); /* "open" id'li elementi seçer */
+const close =
+  document.getElementById("close"); /* "close" id'li elementi seçer */
+const container =
+  document.querySelector(
+    ".container"
+  ); /* ".container" sınıfına sahip elementi seçer */
 
-open.addEventListener("click", ()=>{
-    container.classList.add("show-nav");
+open.addEventListener("click", () => {
+  container.classList.add(
+    "show-nav"
+  ); /* "container" elementine "show-nav" sınıfını ekler */
 });
 
-close.addEventListener("click",()=>{
-    container.classList.remove("show-nav")
+close.addEventListener("click", () => {
+  container.classList.remove(
+    "show-nav"
+  ); /* "container" elementinden "show-nav" sınıfını kaldırır */
 });
-
